4f6c9f6fcbecf651b107aa51f67779233215efb8
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-11-04 22:18:12 +0530

    add mysql support

--- commands/backup.js
+++ commands/backup.js
@@ -1,11 +1,13 @@
 import getConfig from "../lib/getConfig.js";
 import { MongoDBBackup } from "../lib/backups/mongodbBackup.js";
+import { MySQLBackup } from "../lib/backups/mySqlBackup.js";
 
 function getBackupClass(config) {
   switch (config.type) {
     case "mongodb":
       return new MongoDBBackup(config);
-    // Add cases for other databases
+    case "mysql":
+      return new MySQLBackup(config);
     default:
       throw new Error("Unsupported database type");
   }
